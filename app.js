848461b2993e87c3702cbeceae4a661f32ebd614
Anitor <anitorabraham@gmail.com>
2022-11-27 21:29:38 +0100

    database setup completed

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+const express = require("express"); //import express.
+const app = express(); // run express.
+
+//database
+const connectDB = require("./db/connect");
+const port = process.env.port || 3000; //define port.
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on ${port}`)); // listen on the server.
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
