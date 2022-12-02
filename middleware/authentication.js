9e35acaf4ae876005c4804b7d4778f386e8042bd
Anitor <anitorabraham@gmail.com>
2022-12-02 21:38:48 +0100

    done with user authentication

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,14 +2,22 @@ const CustomError = require("../errors");
 const { isTokenValid } = require("../utils");
 
 const authenticateUser = async (req, res, next) => {
-    // we check for signed cookies
+  // we check for signed cookies from the request.
   const token = req.signedCookies.token;
 
   if (!token) {
-    console.log("error, no token present");
-  } else {
-    console.log("token present");
+    throw new CustomError.UnauthenticatedError("Authentication Invalid");
+  }
+  //   we are making the same mistakes that we made, in the time
+
+  try {
+    const payload = isTokenValid({ token });
+    const { name, userId, role } = payload;
+    req.user = { name, userId, role };
+    console.log(payload);
     next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError("Authentication Invalid");
   }
 };
 
